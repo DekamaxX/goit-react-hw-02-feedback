634f3ef011ac8f21fb275494e4130e58f814ae6e
DekamaxX <dobosz.mateusz1994@gmail.com>
2024-06-05 14:52:53 +0200

    change feedback

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,1 +1,22 @@
-v
+import PropTypes from 'prop-types';
+import css from './FeedbackOptions.module.css';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div className={css.container}>
+      {options.map(option => (
+        <button
+          className={css.button}
+          key={option}
+          onClick={() => onLeaveFeedback(option)}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  option: PropTypes.string,
+};
